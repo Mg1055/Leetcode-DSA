fc9d45b7210bf21359a4c4461bdca490c67376ca
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-09-30 14:06:55 +0530

    Time: 3 ms (65.42%), Space: 9.6 MB (100%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = -1;
+        int m = matrix.size(), n = matrix[0].size();
+        int l = 0, r = m-1;
+        while(l <= r){
+            int mid = l+(r-l)/2;
+            if(matrix[mid][0] == target)
+                return true;
+            else if(matrix[mid][0] > target){
+                r = mid-1;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        row = r;
+        if(r >= 0){
+            l = 0, r = n-1;
+            while(l <= r){
+                int mid = l + (r-l)/2;
+                if(matrix[row][mid] < target)
+                    l = mid+1;
+                else if(matrix[row][mid] > target)
+                    r = mid-1;
+                else
+                    return true;
+            }
+        }
+        return false;
+    }
+};
