f92e8f50cc14eb54a321544617a7397e93a5bfa6
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-09-30 16:03:04 +0530

    Time: 7 ms (85.25%), Space: 13.9 MB (50.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,11 +1,32 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>um;
-        for(int i = 0; i < nums.size(); i++){
-            if(um.find(target-nums[i]) != um.end())
-                return {um[target-nums[i]], i};
-            um[nums[i]] = i;
+        // hash map
+        // unordered_map<int,int>um;
+        // for(int i = 0; i < nums.size(); i++){
+        //     if(um.find(target-nums[i]) != um.end())
+        //         return {um[target-nums[i]], i};
+        //     um[nums[i]] = i;
+        // }
+        // return {};
+
+
+        // two pointer
+        vector<pair<int,int>>n;
+        pair<int,int>p;
+        for(int i = 0; i < nums.size(); i++)
+            n.push_back({nums[i],i});
+
+        sort(n.begin(),n.end());
+        int l = 0, r = nums.size()-1;
+        while(l < r){
+            int temp = n[l].first + n[r].first;
+            if(temp == target)
+                return {n[l].second, n[r].second};
+            else if(temp > target)
+                r--;
+            else
+                l++;
         }
         return {};
     }
