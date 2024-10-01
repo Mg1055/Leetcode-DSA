b0927f0b90243474fb92cb21ce98c8847d200844
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-10-01 14:09:42 +0530

    Time: 720 ms (10.43%), Space: 284.3 MB (17.71%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
-    void merge(vector<int>& nums, int low, int mid, int high){
-        if(low >= high)    return;
-        vector<int> sorted(high - low + 1, 0);
-        int l = low, r = mid+1, k = 0;
-        while(l <= mid and r <= high)
-            sorted[k++] = nums[l] < nums[r] ? nums[l++] : nums[r++];
-        while (l <= mid) 
-            sorted[k++] = nums[l++];
-        while (r <= high) 
-            sorted[k++] = nums[r++];
-        for (k = 0; k < high - low + 1; k++)
-            nums[k + low] = sorted[k];
+    void merge(vector<int>& arr, int low, int mid, int high){
+        vector<int> temp; 
+        int left = low;      // starting index of left half of arr
+        int right = mid + 1;   // starting index of right half of arr
+
+        //storing elements in the temporary array in a sorted manner//
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else {
+                temp.push_back(arr[right]);
+                right++;
+            }
+        }
+
+        // if elements on the left half are still left //
+        while (left <= mid)
+            temp.push_back(arr[left++]);
+
+        //  if elements on the right half are still left //
+        while (right <= high)
+            temp.push_back(arr[right++]);
+
+        // transfering all elements from temporary to arr //
+        for (int i = low; i <= high; i++)
+            arr[i] = temp[i - low];
     }
 
     void mergeSort(vector<int>&nums, int low, int high){
