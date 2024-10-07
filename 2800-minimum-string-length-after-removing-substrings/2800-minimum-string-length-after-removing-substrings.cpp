150b6d00ca0a7516a1719bbfe712e4914f078f9b
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-10-08 01:22:02 +0530

    Time: 5 ms (59.45%), Space: 10 MB (97.79%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     int minLength(string s) {
-        stack<int>st;
+        // stack<int>st;
 
-        for(int i = 0; i < s.size(); i++){
-            if(st.empty() == true){
-                st.push(s[i]);
-                continue;
-            }
+        // for(int i = 0; i < s.size(); i++){
+        //     if(st.empty() == true){
+        //         st.push(s[i]);
+        //         continue;
+        //     }
 
-            if(s[i] == 'B' && st.top() == 'A')
-                st.pop();
-            else if(s[i] == 'D' && st.top() == 'C')
-                st.pop();
+        //     if(s[i] == 'B' && st.top() == 'A')
+        //         st.pop();
+        //     else if(s[i] == 'D' && st.top() == 'C')
+        //         st.pop();
+        //     else
+        //         st.push(s[i]);
+        // }
+
+        // return st.size();
+
+        int stack_top = 0;
+        for(auto c : s){
+            if(stack_top > 0 && ((s[stack_top - 1] == 'A' && c == 'B') || (s[stack_top - 1] == 'C' && c == 'D')))
+                stack_top--;
             else
-                st.push(s[i]);
+                s[stack_top++] = c;
         }
-
-        return st.size();
+        return stack_top;
     }
 };
