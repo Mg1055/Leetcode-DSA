b791aab4b01e71508967fad4507abdd7c9ebddc5
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-10-08 15:10:29 +0530

    Time: 5 ms (13.37%), Space: 8.5 MB (90.75%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>ans;
-        int n = nums.size();
-        int power2num = pow(2,n);
-
-        for(int i = 0; i < power2num; i++){
-
-            int curr = i;
-            int k = 0;
-            vector<int>temp;
-            while(curr > 0){
-                if(curr%2 != 0){
-                    temp.push_back(nums[n - k - 1]);
-                }
-                curr = curr/2;
-                k++;
+        int n = nums.size(), p = 1 << n;
+        vector<vector<int>> subs(p);
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i >> j) & 1) 
+                    subs[i].push_back(nums[j]);
             }
-            reverse(temp.begin(), temp.end());
-            ans.push_back(temp);
         }
-
-        return ans;
+        return subs;
     }
 };
