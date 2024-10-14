6c5ea3fe0edb15dafea9de159aefc0f82a4c707e
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-10-15 01:03:03 +0530

    Time: 153 ms (95.51%), Space: 73.2 MB (99.25%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     long long maxKelements(vector<int>& nums, int k) {
         long long ans = 0;
-        priority_queue<int> pq(nums.begin(), nums.end());
+        make_heap(nums.begin(), nums.end());
         for(int i = 0; i < k; i++){
-            int x = pq.top();
+            pop_heap(nums.begin(), nums.end());
+            int x = nums.back();
             ans += x;
-            if (x == 1){
+            if(x == 1){
                 ans += (k-1-i);
                 break;
             }
-            pq.pop();
-            pq.push((x + 2) / 3);
+            nums.back() = (x + 2)/3;
+            push_heap(nums.begin(), nums.end());
         }
         return ans;
     }
