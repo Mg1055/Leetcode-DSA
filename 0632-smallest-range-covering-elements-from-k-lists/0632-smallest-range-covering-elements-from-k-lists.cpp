9cdfc370f4c71aa2d0bb0d662a94f80cf735258c
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-10-15 22:07:54 +0530

    Time: 212 ms (7.41%), Space: 40.7 MB (5.22%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -18,13 +18,13 @@ public:
 
     vector<int> smallestRange(vector<vector<int>>& nums) {
         int k = nums.size();
-        priority_queue< vector<int> >maxh;
+        // priority_queue< vector<int> >maxh;
         priority_queue< vector<int>, vector< vector<int> >, greater< vector<int> > >minh;
-
+        int l = 0, r = INT_MIN;
         for(int i = 0; i < k; i++){
-            maxh.push({nums[i][0], 0, i});
+            // maxh.push({nums[i][0], 0, i});
             minh.push({nums[i][0], 0, i});
-
+            r = max(r, nums[i][0]);
         }
 
         // while(!minh.empty()){
@@ -38,9 +38,10 @@ public:
         // }
         // return {};
 
-        int l = minh.top()[0];
-        int r = maxh.top()[0];
+        l = minh.top()[0];
+        // int r = maxh.top()[0];
         int ans_interval_size = r - l;
+        int currL = 0, currR = r;
 
         while(true){
             int num = minh.top()[0];
@@ -54,10 +55,11 @@ public:
             // printmaxheap(maxh);
             minh.pop();
             ind++;
+            
             minh.push({nums[list_no][ind], ind, list_no});
-            maxh.push({nums[list_no][ind], ind, list_no});
+            // maxh.push({nums[list_no][ind], ind, list_no});
 
-            int currL = minh.top()[0], currR = maxh.top()[0];
+            currL = minh.top()[0], currR = max(currR, nums[list_no][ind]);
             int curr_interval_size = currR - currL;
             if(curr_interval_size < ans_interval_size){
                 cout << "currL : " << currL << " " << "currR : " << currR << endl;
