c8f044893c77b723712697b2efbbbf7629be3a4a
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-10-15 20:20:52 +0530

    Time: 27 ms (89.47%), Space: 15.4 MB (89.89%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -3,13 +3,12 @@ public:
     long long minimumSteps(string s) {
         long long ans = 0;
         // int zeroes = 0;
-        int ones = 0;
+        // int ones = 0;
         int n = s.size();
-        for(auto c : s){
-            if(c == '0')
-                ans += ones;
-            else
-                ones++;
+        int l = 0, r = 0;
+        for(;r < n; r++){
+            if(s[r] == '0')
+                ans += r-(l++);
         }
         return ans;
     }
