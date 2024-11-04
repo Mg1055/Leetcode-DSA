d7c3a0ecf06b544cf37b3cdd0e7a8be5f1558402
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-11-04 21:56:49 +0530

    Time: 0 ms (100%), Space: 8.5 MB (33.09%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        int fact = 1;
+        vector<int>nums;
+        for(int i = 1; i < n; i++){
+            fact *= i;
+            nums.push_back(i);
+        }
+        nums.push_back(n);
+        string ans = "";
+        k--;
+        while(true){
+            int new_seq_no = k/fact;
+            ans += to_string(nums[new_seq_no]);
+            nums.erase(nums.begin() + new_seq_no);
+            if(nums.size() == 0)
+                break;
+            k = k%fact; // new position of k
+            fact = fact/nums.size();
+        }
+        return ans;
+    }
+};
