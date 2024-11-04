1e2e7a5466614f7e65b6ff98860318400427a939
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-11-04 18:32:42 +0530

    Time: 3 ms (66.45%), Space: 10 MB (57.55%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    
+
+    vector<vector<string>>ans;
+
     bool is_Valid(vector<string> &board, int r, int c){
         int n = board.size();
         for(int i = 0; i < r; i++){
@@ -18,26 +20,24 @@ public:
         return true;
     }
     
-    void solve(vector<vector<string>> &ans, vector<string> &board, int r){
+    void solve(vector<string>& board, int row){
         int n = board.size();
-        if(r == n){
+        if(row == n){
             ans.push_back(board);
             return;
         }
-        for(int c = 0; c < n; c++){
-            if(is_Valid(board,r,c) == true){
-                board[r][c] = 'Q';
-                solve(ans,board,r+1);
-                board[r][c] = '.';
+        for(int col = 0; col < n; col++){
+            if(is_Valid(board, row, col) == true){
+                board[row][col] = 'Q';
+                solve(board, row+1);
+                board[row][col] = '.';
             }
         }
     }
-    
+
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>ans;
-        vector<string>board(n,string(n,'.'));
-        solve(ans,board,0);
-        return ans;
+        vector<string>board(n, string(n,'.'));
+        solve(board, 0);
+        return ans;        
     }
-    
 };
