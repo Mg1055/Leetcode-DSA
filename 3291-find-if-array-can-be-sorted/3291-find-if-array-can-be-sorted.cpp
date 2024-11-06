967851384f3df2115b12426a30003e7158a5367c
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-11-06 20:46:23 +0530

    Time: 0 ms (100%), Space: 31.3 MB (49.73%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -1,35 +1,58 @@
 class Solution {
 public:
-
-    int setBits(int n){
-        int k = 0;
+    int countSetBits(int n){
+        int setBits = 0;
         while(n > 0){
             n = n & n-1;
-            k++;
+            setBits++;
         }
-        return k;
+        return setBits;
     }
 
     bool canSortArray(vector<int>& nums) {
-        int n = nums.size();
         
+        int n = nums.size();
+        int curr_min = nums[0], curr_max = nums[0];
+        int prev_max = INT_MIN;
+
         for(int i = 0; i < n; i++){
-            bool swapped = false;
-            for(int j = 0; j < n-i-1; j++){
-                if(nums[j] > nums[j+1]){
-                    if(setBits(nums[j]) == setBits(nums[j+1])){
-                        swapped = true;
-                        swap(nums[j], nums[j+1]);
-                    }
-                    else{
-                        return false;
-                    }
-                }
+            if(countSetBits(nums[i]) == countSetBits(curr_min)){
+                curr_min = min(curr_min, nums[i]);
+                curr_max = max(curr_max, nums[i]);
             }
-            if(swapped == false)
-                break;
+            else{
+                if(curr_min < prev_max)
+                    return false;
+                prev_max = curr_max;
+                curr_min = nums[i], curr_max = nums[i];
+            }
         }
         
+        if(curr_min < prev_max)
+            return false;
         return true;
+
+        // bubble sort
+
+        // int n = nums.size();
+        
+        // for(int i = 0; i < n; i++){
+        //     bool swapped = false;
+        //     for(int j = 0; j < n-i-1; j++){
+        //         if(nums[j] > nums[j+1]){
+        //             if(countSetBits(nums[j]) == countSetBits(nums[j+1])){
+        //                 swapped = true;
+        //                 swap(nums[j], nums[j+1]);
+        //             }
+        //             else{
+        //                 return false;
+        //             }
+        //         }
+        //     }
+        //     if(swapped == false)
+        //         break;
+        // }
+        
+        // return true;
     }
 };
