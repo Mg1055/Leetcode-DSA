cdd59f5efd722b0ebc4b4acfb4d97d4ed662fbad
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-11-07 12:57:50 +0530

    Time: 0 ms (100%), Space: 12.3 MB (83.07%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& nums, int k) {
-        int n = nums.size();
-        for(auto x : nums){
-            if(x <= k)
-                k++;
-            else
-                break;
-        }
+        // O(n) solution
         
-        return k;
+        // int n = nums.size();
+        // for(auto x : nums){
+        //     if(x <= k)
+        //         k++;
+        //     else
+        //         break;
+        // }
+        
+        // return k;
+
+
+        // Binary search
+        int n = nums.size();
+        int l = 0, r = n-1;
+
+        while(l <= r){
+            int mid = (r+l)/2;
+            int missing = nums[mid] - (mid+1);
+
+            if(missing < k)
+                l = mid+1;
+            else
+                r = mid-1;
+        }
+
+        return r+k+1;
     }
 };
