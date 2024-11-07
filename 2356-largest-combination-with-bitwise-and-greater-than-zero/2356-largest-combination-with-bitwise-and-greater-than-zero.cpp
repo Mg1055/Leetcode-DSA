2d5b99f593183abe4a0ac6a2cb978aa4bf1af1bd
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-11-07 18:34:33 +0530

    Time: 9 ms (96.27%), Space: 60.3 MB (32.98%) - LeetHub

---
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int largestCombination(vector<int>& arr) {
+        int n = arr.size();
+        vector<int>setBits(24,0);
+        for(auto x : arr){
+            int k = x;
+            int i = 0;
+            while(k > 0){
+                setBits[i] += k%2;
+                k = k/2;
+                i++;
+            }
+        }
+
+        int ans = 1;
+        for(auto x : setBits)
+            ans = max(x, ans);
+        
+        return ans;
+    }
+};
