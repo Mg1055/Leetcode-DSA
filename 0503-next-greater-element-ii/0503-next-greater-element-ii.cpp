34730216156a11ede426f2db70b4910332ac7a8f
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-11-08 20:42:19 +0530

    Time: 7 ms (52.21%), Space: 26.8 MB (56.35%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,20 +4,13 @@ public:
         int n = nums.size();
         vector<int>nge(n, -1);
         stack<int>st;
-        int i = n-1;
-        for(; i >= 0; i--){
-            while(!st.empty() && st.top() <= nums[i])
+        
+        for(int i = (2*n) - 1; i >= 0; i--){
+            while(!st.empty() && st.top() <= nums[i%n])
                 st.pop();
-            nge[i] = (st.empty()) ? -1 : st.top();
-            st.push(nums[i]);
-        }
-
-        i = n-1;
-        for(; i >= 0; i--){
-            while(!st.empty() && st.top() <= nums[i])
-                st.pop();
-            nge[i] = (st.empty()) ? -1 : st.top();
-            st.push(nums[i]);
+            if(!st.empty()) 
+                nge[i%n] = st.top();
+            st.push(nums[i%n]);
         }
 
         return nge;
