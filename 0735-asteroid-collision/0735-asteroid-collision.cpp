06fe12b30ffd974ebc8b5df4e6cf3fd04d31d7fd
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-11-09 13:16:03 +0530

    Time: 0 ms (100%), Space: 21 MB (18.09%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& ast) {
+        int n = ast.size();
+        stack<int> st;
+        for(int i = 0; i < n; i++) {
+            if(ast[i] > 0 || st.empty()) {
+                st.push(ast[i]);
+            }
+            else {
+                while(!st.empty() and st.top() > 0 and st.top() < abs(ast[i])) {
+                    st.pop();
+                }
+                if(!st.empty() and st.top() == abs(ast[i])) {
+                    st.pop();
+                }
+                else {
+                    if(st.empty() || st.top() < 0) {
+                        st.push(ast[i]);
+                    }
+                }
+            }
+        }
+
+        n = st.size();
+        vector<int> ans(n);
+        for(int i = n-1; i >= 0; i--){
+            ans[i] = st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
