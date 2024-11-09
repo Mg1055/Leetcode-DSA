1ce68d1682cd509906f100196b12a388a3cdc20b
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-11-09 17:12:16 +0530

    Time: 4 ms (52.88%), Space: 10.1 MB (81.53%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        stack<int>st;
-        for(int i = 0; i < num.length(); i++){
-            while(k && !st.empty() && (num[i] - '0') < st.top()){
-                st.pop();
+        string ans = "";
+        for(char &c : num){
+            while(ans.size() && ans.back() > c && k > 0){
+                ans.pop_back();
                 k--;
             }
-            st.push(num[i] - '0');
+            if(ans.size() || c != '0')
+                ans.push_back(c);
         }
 
-        // if [12345] and k = 2, so no removal in the while loop. So remove now
-        while(k-- > 0)
-            st.pop();
-
-        string ans = "";
-        while(!st.empty()){
-            ans.push_back((st.top() + '0'));
-            st.pop();
-        }
-
-        // trim leading zeroes
-        while(ans.size() != 0 && ans.back() == '0')
+        while(ans.size() && k--)
             ans.pop_back();
-
-        reverse(ans.begin(),ans.end());
+        
         return ans.empty() ? "0" : ans;
     }
 };
