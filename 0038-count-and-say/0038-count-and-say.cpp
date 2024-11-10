857fc4cf7cfbeb8c732009abc5380cbf9d269e30
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-11-10 21:11:27 +0530

    Time: 4 ms (65.07%), Space: 10.4 MB (62.72%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        if(n == 1)
-            return "1";
-        string prev = countAndSay(n-1);
-        char c = prev[0];
-        int cnt = 1;
-
-        string res = "";
-        for (int i = 1; i <= prev.size(); i++) {
-            if (prev[i] == c) {
-                cnt++;
-            } 
-            else {
-                res += to_string(cnt);
-                res.push_back(c);
-                c = prev[i];
-                cnt = 1;
+        if (n == 0) return "";
+        string res = "1";
+        n--;
+        while(n){
+            string cur = "";
+            for(int i = 0; i < res.size(); i++){
+                int count = 1;
+                while((i + 1 < res.size()) && (res[i] == res[i + 1])){
+                    count++;    
+                    i++;
+                }
+                cur += to_string(count) + res[i];
             }
+            res = cur;
+            n--;
         }
-
         return res;
     }
 };
