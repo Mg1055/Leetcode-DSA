fe13f12bc37b795e0f7b34a00f039cd1a42ba16b
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-11-11 16:25:00 +0530

    Time: 0 ms (100%), Space: 7.8 MB (66.52%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+
+    void findLPS(string pat, vector<int>&lps){
+        int i = 1, j = 0;
+        int m = pat.size();
+        while(i < m){
+            if(pat[i] == pat[j]){
+                lps[i] = j+1;
+                i++;j++;
+            }
+            else if(j > 0){
+                j = lps[j-1];
+            }
+            else{
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+
+    int strStr(string text, string pat) {
+        int n = text.size(), m = pat.size();
+
+        // Rabin karp
+        // int d = 256;
+        // int q = 101; // prime for hashing
+        // int h = 0; // base for updating
+        // for(int i = 0; i < m-1; i++)
+        //     h = (h * d) % q;
+        
+        // int p = 0, t = 0;  // hash of pattern (p) and text (t)
+        // for(int i = 0; i < m; i++){
+        //     p = (d * p + pat[i]) % q;
+        //     t = (d * t + text[i]) % q;
+        // }
+
+        // for(int i = 0; i < n-m+1; i++){
+        //     if(p == t){
+        //         int j;
+        //         for(j = 0; j < m; j++){
+        //             if (text[i + j] != pat[j])
+        //                 break;
+        //         }
+        //         if(j == m)
+        //             return i; // pattern found
+        //     }
+    
+        //     if(i < n - m){
+        //         t = (d * (t - text[i] * h) + text[i + m]) % q;
+        //         if(t < 0)
+        //             t += q;
+        //     }
+        // }
+        // return -1;
+
+        // KMP
+        vector<int>lps(m);
+        findLPS(pat, lps);
+        int i = 0, j = 0;
+        while(i < n && j < m){
+            if(text[i] == pat[j]){
+                i++; j++;
+            } 
+            else if(j > 0){
+                j = lps[j-1];
+            }
+            else{
+                i++;
+            }
+        }
+
+        return j == pat.size() ? i-j : -1;
+    }
+};
