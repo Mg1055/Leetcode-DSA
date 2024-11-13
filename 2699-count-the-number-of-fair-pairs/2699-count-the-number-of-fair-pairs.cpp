475b320534dd7c2ec7544fd5eb3bcb3a49194915
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-11-13 12:05:26 +0530

    Time: 34 ms (83.93%), Space: 60.2 MB (94.04%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -18,13 +18,11 @@ public:
                 auto it = upper_bound(nums.begin()+l, nums.begin()+r, k1);
                 int n1 = (--it) - nums.begin() - l + 1;
                 ans += n1;
-                cout << "l :" << l << " r: " << r << " ans: " << ans << endl; 
 
                 int k2 = lower - nums[l];
                 it = lower_bound(nums.begin()+l+1, nums.begin()+r+1, k2);
                 int n2 = r - (it - nums.begin()) + 1;
                 ans += n2;
-                cout << "l :" << l << " r: " << r << " ans: " << ans << endl; 
 
                 if(n1 > 0 && n2 > 0)
                     ans--;
