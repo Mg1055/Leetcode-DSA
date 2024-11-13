d4fff2456a004dcd1568e7df4b5964bbd17f3a03
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-11-13 17:49:26 +0530

    Time: 0 ms (100%), Space: 17.6 MB (80.03%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -11,10 +11,11 @@
  */
 class Solution {
 public:
-    
+
     int maxDepth(TreeNode* root) {
-        if (root == NULL) 
-            return 0; 
-		return max(maxDepth(root->left)+1, maxDepth(root->right)+1);
+        if(root == NULL)    return 0;
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
+        return 1 + max(lh, rh);
     }
 };
