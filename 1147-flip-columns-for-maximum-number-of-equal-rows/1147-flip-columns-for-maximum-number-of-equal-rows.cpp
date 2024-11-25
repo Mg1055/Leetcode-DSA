d48665ec4b299ba44022791d094ad3cd14207369
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-11-26 00:41:27 +0530

    Time: 109 ms (24.49%), Space: 84.5 MB (8.65%) - LeetHub

--- 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.cpp
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/1147-flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -3,18 +3,18 @@ public:
     int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
         unordered_map<string, int> mp;
 
-		for (auto& r : matrix) {
-			string s(r.size(), 'T');
-			for (int i = 1; i < r.size(); i++) {
-				if (r[i] != r[0]) s[i] = 'F';
-			}
-			mp[s]++;
+		for (auto row : matrix) {
+            string pattern = "";
+            if (row[0] == 0)
+                for (int bit : row) pattern += to_string(bit);
+            else
+                for (int bit : row) pattern += to_string(bit ^ 1);
+			mp[pattern]++;
 		}
 
 		int ans = 0;
-		for (auto& p : mp)
+		for (auto p : mp)
 			ans = max(ans, p.second);
-		
 		return ans;
     }
 };
