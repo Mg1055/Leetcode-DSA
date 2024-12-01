1383f670081123b71427aeed72550516ce698e70
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-12-01 12:06:01 +0530

    Time: 0 ms (100%), Space: 14.9 MB (10.05%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -2,15 +2,11 @@ class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
         unordered_map<int,int>mp;
-        int n = arr.size();
-        for(int i = 0; i < n; i++)
-            mp[arr[i]] = i;
-        for(int i = 0; i < n; i++){
-            int doubleNum = arr[i]*2;
-            if(mp.find(doubleNum) != mp.end()){
-                if(mp[doubleNum] != i)
-                    return true;
-            }
+        for(int i = 0; i < arr.size(); i++)  mp[arr[i]] = i;
+        
+        for(int i = 0; i < arr.size(); i++){
+            if(mp.find(arr[i]*2) != mp.end() && mp[arr[i]*2] != i)
+                return true;
         }
         return false;
     }
