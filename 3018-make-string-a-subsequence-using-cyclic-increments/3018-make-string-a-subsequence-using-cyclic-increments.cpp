435f74e6dfae40bf5544cc43865549a3218d395f
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-12-11 01:07:41 +0530

    Time: 4 ms (59.64%), Space: 17.1 MB (36.51%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    bool canMakeSubsequence(string source, string target) {
-        int targetIdx = 0;
-        int targetLen = target.length();
-        for (char currChar : source) {
-            if (targetIdx < targetLen && (target[targetIdx] - currChar + 26) % 26 <= 1) {
-                targetIdx++;
-            }
+    bool canMakeSubsequence(string str1, string str2) {
+        int i = 0, j = 0;
+        int n1 = str1.length(), n2 = str2.length();
+        
+        while(i < n1 && j < n2) {
+            if((str2[j] - str1[i] + 26) % 26 <= 1)
+                j++;
+            i++;
         }
-        return targetIdx == targetLen;
+        return j == n2;
     }
 };
