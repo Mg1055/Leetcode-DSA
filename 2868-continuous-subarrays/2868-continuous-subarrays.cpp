68e10f0d4d05ac45f172d7471393963a4bccb011
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-12-14 17:55:58 +0530

    Time: 79 ms (93.11%), Space: 111.6 MB (94.68%) - LeetHub

---
+++ 2868-continuous-subarrays/2868-continuous-subarrays.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        int l = 0, r = 0,j = 0;
+        long long ans = 0;
+            
+        for(int i = 0; i < nums.size(); i++){
+            if(i == 0){
+                ans = ans + 1;
+                l = nums[i] - 2; 
+                r = nums[i] + 2; 
+            }
+            else if(nums[i] >= l && nums[i] <= r){                
+                l = max(l,nums[i]-2);
+                r = min(r,nums[i]+2);
+                ans = ans + (i-j+1); 
+                                
+            }
+            else{          
+                j = i-1;
+                l = nums[i] - 2 , r = nums[i] + 2;
+                while(nums[j]  >= nums[i] - 2 && nums[j] <= nums[i] + 2){        
+                    l = max(l,nums[j] - 2);  
+                    r = min(r,nums[j] + 2);
+                    j--;           
+                }
+                j++;          
+                ans = ans + (i-j+1);           
+            }
+        }      
+        return ans;    
+    }
+};
