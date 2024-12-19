4b2b3c17e702c89b8f0fa4324e717b56c7b4f34f
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-12-19 15:14:44 +0530

    Time: 0 ms (100%), Space: 9.2 MB (20.72%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
         int n = arr.size();
-        vector<int>maxE(n);
-        maxE[0] = arr[0];
-        for(int i = 1; i < n; i++)
-            maxE[i] = max(maxE[i-1], arr[i]);
-
-        int ans = 0;
+        int ans = 0, maxE = 0;
         for(int i = 0; i < n; i++){
-            if(maxE[i] == i)
+            maxE = max(maxE, arr[i]);
+            if(maxE == i)
                 ans++;
         }
         return ans;
