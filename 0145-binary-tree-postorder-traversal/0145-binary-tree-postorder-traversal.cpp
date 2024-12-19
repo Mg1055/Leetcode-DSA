9270e3fb1082f0f35f1fb933ac8305d7fea585a0
Mg1055 <62701211+Mg1055@users.noreply.github.com>
2024-12-19 19:10:48 +0530

    Time: 0 ms (100%), Space: 11 MB (21.67%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -16,15 +16,29 @@ public:
         
         vector<int>ans;
         stack<TreeNode*>st;
-        st.push(root);
-        while(!st.empty()){
-            TreeNode* node = st.top();
-            st.pop();
-            ans.push_back(node->val);
-            if(node->left)  st.push(node->left);
-            if(node->right) st.push(node->right);
+        TreeNode* curr = root;
+        while(curr != NULL || !st.empty()){
+            if(curr != NULL){
+                st.push(curr);
+                curr = curr->left;
+            }
+            else{
+                TreeNode* temp = st.top()->right;
+                if(temp == NULL){
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    while(!st.empty() && temp == st.top()->right){
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                else{
+                    curr = temp;
+                }
+            }
         }
-        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
